7fb88e7ce8c86bb86bf89b9c409cdaba79a897da
Trent Garner <trentongarner.13@icloud.com>
2021-01-11 18:10:49 -0700

    Created form for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    render plain: params[:article]
+  end
+
 end
