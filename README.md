1f563db27f97fca351352ae9a9a506ea73beed8b
Trent Garner <trentongarner.13@icloud.com>
2021-02-05 15:43:34 -0700

    Add edit template and update action

--- README.md
+++
@@ -1,3 +0,0 @@
-ActionView::Base.field_error_proc = proc.new do |html_tag, instance|
-  html_tag.html_safe
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:notice] = "#{@user.username} edited!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "#{@user.username}, your account has been updated!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
