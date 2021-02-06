3820c1ae15669be039aa08183047bed569f1631d
Trent Garner <trentongarner.13@icloud.com>
2021-02-06 16:11:47 -0700

    Add users index and navbar users route/link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,26 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
-    if @user.save
-      flash[:notice] = "#{@user.username} edited!"
-      redirect_to articles_path
-    else
-      render 'new'
-    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "#{@user.username}, your account has been updated!"
+      flash[:notice] = "Your account information was successfully updated"
       redirect_to articles_path
     else
       render 'edit'
@@ -27,7 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to Alpha Blog, #{@user.username}!"
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
       render 'new'
@@ -35,7 +38,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+  has_many :articles, dependent: :destroy
   validates :username, presence: true,
                       uniqueness: { case_sensitive: false },
                       length: { minimum: 3, maximum: 25 }
@@ -8,5 +9,5 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false },
                       length: { maximum: 105 },
                       format: { with: VALID_EMAIL_REGEX }
- has_secure_password
+  has_secure_password
 end
