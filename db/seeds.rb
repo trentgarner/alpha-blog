3c214c1cb8a54fd294b32410c34c20216dddb67c
Trent Garner <trentongarner.13@icloud.com>
2021-03-10 00:35:53 -0700

    Add seeds for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new
+user.username = "Trent"
+user.email = "tg@example.com"
+user.password = "password123"
+user.admin = true
